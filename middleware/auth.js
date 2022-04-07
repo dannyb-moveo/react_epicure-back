043b10f72248957cb89fed13d4c231298d56802f
dannyb-moveo <dannyb@moveo.co.il>
2022-04-07 15:29:23 +0300

    added get auth

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,23 +1,21 @@
 import { UnauthenticatedError } from "../errors/index.js";
 import jwt from "jsonwebtoken";
+import { register } from "../controllers/authController.js";
 
 const auth = async (req, res, next) => {
-  console.log(req.method);
-  console.log(req.path);
-  console.log(req.url);
-  console.log(req.baseUrl);
-  console.log(req.hostname);
-  console.log(req.get("host"));
-  console.log("hi");
+  // console.log(req.method);
+  // console.log(req.path);
+  // console.log(req.url);
+  // console.log(req.baseUrl);
+  // console.log(req.hostname);
+  // console.log(req.get("host"));
+  // console.log("hi");
 
-  const authorizedUrls = [
-    "/api/v1/chefs",
-    "/api/v1/restaurants",
-    "/api/v1/dishes",
-    "/api/v1/chefs",
-  ];
+  const authorizedUrl = "ec2-54-93-233-115.eu-central-1.compute.amazonaws.com";
 
-  // const url = authorizedUrls.find(url => url === )
+  if (req.get === authorizedUrl && req.method === "GET") {
+    next();
+  }
 
   const authHeader = req.headers.authorization;
   if (!authHeader || !authHeader.startsWith("Bearer ")) {
