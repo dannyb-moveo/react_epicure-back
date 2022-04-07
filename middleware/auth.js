d01023c97d1b6b31da2419c7c19ea369f35c9c9e
dannyb-moveo <dannyb@moveo.co.il>
2022-04-07 14:53:34 +0300

    testing hostname in auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,6 +2,23 @@ import { UnauthenticatedError } from "../errors/index.js";
 import jwt from "jsonwebtoken";
 
 const auth = async (req, res, next) => {
+  console.log(req.method);
+  console.log(req.path);
+  console.log(req.url);
+  console.log(req.baseUrl);
+  console.log(req.hostname);
+  console.log(req.get("host"));
+  console.log("hi");
+
+  const authorizedUrls = [
+    "/api/v1/chefs",
+    "/api/v1/restaurants",
+    "/api/v1/dishes",
+    "/api/v1/chefs",
+  ];
+
+  // const url = authorizedUrls.find(url => url === )
+
   const authHeader = req.headers.authorization;
   if (!authHeader || !authHeader.startsWith("Bearer ")) {
     throw new UnauthenticatedError("Authentication invalid");
